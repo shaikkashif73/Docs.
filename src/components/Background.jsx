5b0e9730ed97dbc6544c0f19fca26302e0ae68e7
Shaik Kashif <kashifcodes10@gmail.com>
2026-06-11 04:14:55 +0530

    Creating Foreground Component  and Card Component

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,12 +3,14 @@ import React from "react";
 function Background() {
   return (
     <>
-      <div className=" absolute top-[5%] w-full py-10 flex justify-center text-zinc-600 text-xl font-semibold ">
-        Documents.
+      <div className=" fixed  z-[2] w-full h-screen">
+        <div className=" absolute top-[5%] w-full py-10 flex justify-center text-zinc-600 text-xl font-semibold ">
+          Documents.
+        </div>
+        <h1 className="text-[13vw] top-1/2 left-1/2 -translate-y-[50%] font-semibold text-zinc-900 -translate-x-[50%] leading-none tracking-tighter absolute">
+          Docs.
+        </h1>
       </div>
-      <h1 className="text-[13vw] top-1/2 left-1/2 -translate-y-[50%] font-semibold text-zinc-900 -translate-x-[50%] leading-none tracking-tighter absolute">
-        Docs.
-      </h1>
     </>
   );
 }
---
+++ src/components/Foreground.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "./Card";
+
+function Foreground() {
+  return (
+    <div className="w-full h-full fixed z-[3] top-0 left-0 ">
+      <Card />
+    </div>
+  );
+}
+
+export default Foreground;
