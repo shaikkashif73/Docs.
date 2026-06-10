a17566427299116bd1cbe841d07c32e9e0551656
Shaik Kashif <kashifcodes10@gmail.com>
2026-06-11 04:00:01 +0530

    Creating the Background for our Notes Apps

---
+++ src/components/Background.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Background() {
+  return (
+    <>
+      <div className=" absolute top-[5%] w-full py-10 flex justify-center text-zinc-600 text-xl font-semibold ">
+        Documents.
+      </div>
+      <h1 className="text-[13vw] top-1/2 left-1/2 -translate-y-[50%] font-semibold text-zinc-900 -translate-x-[50%] leading-none tracking-tighter absolute">
+        Docs.
+      </h1>
+    </>
+  );
+}
+
+export default Background;
