a850b698bd2d431e5c31bd4ab5dfef715518652e
Shaik Kashif <kashifcodes10@gmail.com>
2026-06-11 04:36:14 +0530

    Making the Elements in Card Component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { FaRegFileAlt } from "react-icons/fa";
+import { LuDownload } from "react-icons/lu";
 
 function Card() {
-  return <div className="w-60 h-72 rounded-[20px] bg-zinc-900/90 "></div>;
+  return (
+    <div className=" relative w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white overflow-hidden px-8 py-10 ">
+      <FaRegFileAlt />
+      <p className="text-sm leading-tight mt-5 font-semibold">
+        Lorem ipsum dolor sit amet consectetur adipisicing.
+      </p>
+      <div className="footer absolute bottom-0  w-full py-3 px-8  left-0">
+        <div className="flex items-center justify-between mb-5">
+          <h5>.4mb</h5>
+          <span className="w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center">
+            <LuDownload size=".7em" color="#fff" />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
